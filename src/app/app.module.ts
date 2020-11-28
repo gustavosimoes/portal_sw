8551362c2cbf0c57fe1f84af8a660a49d943df20
Gustavo Simões <gustavoteixeira@gec.inatel.br>
2020-11-28 14:19:21 -0300

    Inserting cards in the Home.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { BodyHomeComponent } from './components/template/body-home/body-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyHomeComponent
   ],
   imports: [
     BrowserModule,
