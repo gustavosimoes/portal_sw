4feaf8b991d6ff0923e0efac57f7cd697e496ebf
Gustavo Simões <gustavoteixeira@gec.inatel.br>
2020-11-29 20:17:34 -0300

    Insert table of the characters

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -4,6 +4,27 @@ import { Title } from '@angular/platform-browser';
 import { CharactersService } from './characters.service';
 import { Observable } from 'rxjs';
 
+export class Character {
+  constructor(
+    public name: string,
+    public height: number,
+    public mass: number,
+    public hair_color: string,
+    public skin_color: string,
+    public eye_color: string,
+    public birth_year: string,
+    public gender: string,
+    public homeworld: string,
+    public films: string[],
+    public species: string[],
+    public vehicles: string[],
+    public starships: string[],
+    public created: string[],
+    public edited: string[],
+    public url: string[]
+  ) { }
+}
+
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
@@ -11,16 +32,29 @@ import { Observable } from 'rxjs';
 })
 export class CharactersComponent implements OnInit {
   @Input()
-  characters: Observable<any>;
+  page = 0;
+  charactersMatrix: Character[][] = [];
+  characters: Character[] = this.charactersMatrix[0];
 
 
   constructor(private title: Title, private charactersService: CharactersService) {
     this.title.setTitle('Personagens');
-    this.characters = charactersService.getAllCharacters();
-    console.log(this.characters);
+  }
+
+  async getAllCharacters() {
+    this.charactersMatrix = await this.charactersService.getAllCharacters();
+    this.attPage(1);
+  }
+
+  attPage(newPage: any) {
+    if (!((this.page == 1 && newPage == -1) || (this.page == 9 && newPage == 1))) {
+      this.page = this.page + newPage;
+    }
+    this.characters = this.charactersMatrix[this.page - 1];
+    console.log(this.page);
   }
 
   ngOnInit(): void {
+    this.getAllCharacters();
   }
-
 }
--- src/app/pages/characters/characters.service.ts
+++ src/app/pages/characters/characters.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Character } from './characters.component';
 
 
 @Injectable({
@@ -8,10 +9,20 @@ import { Observable } from 'rxjs';
 })
 export class CharactersService {
 
-  private readonly URL = 'https://swapi.dev/api/people';
+  private readonly page_1 = 'https://swapi.dev/api/people';
 
+
+  characters: Character[] = [];
+  charactersMatriz: Character[][] = [];
   constructor(private http: HttpClient) { }
-  getAllCharacters(): Observable<any> {
-    return this.http.get(this.URL);
+  getAllCharacters(): Character[][] {
+    this.http.get<any>(this.page_1).subscribe(res =>
+      this.charactersMatriz[0] = res.results);
+    for(let page = 2; page<=9; page++){
+      this.http.get<any>(`https://swapi.dev/api/people/?page=${page}`).subscribe(res =>
+        this.charactersMatriz[page-1] = res.results);
+    }
+    console.log(this.charactersMatriz);
+    return this.charactersMatriz;
   }
 }
